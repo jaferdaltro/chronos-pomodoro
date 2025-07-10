ad3812a451a495c7139d60f5266f84550a36a910
Jafer Daltro <jafer.daltro@gmail.com>
2025-07-10 17:12:35 -0300

    Create main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
-
 import './styles/global.css';
 import './styles/theme.css';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>This is a simple React application.</p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
