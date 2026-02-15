a1e6a510a9e26dd680eb00adfcfdfbc9b24acf78
Jafer Daltro <jafer.daltro@gmail.com>
2026-02-15 10:09:29 -0300

    Add default input

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo';
 import './styles/global.css';
 import './styles/theme.css';
 import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -18,7 +19,28 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
-
+      <Container>
+        <form action=''>
+          <div className='formRow'>
+            <DefaultInput
+              id='label'
+              type='text'
+              labelText='qq coisa'
+              placeholder='Digite algo'
+            />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>ciclos</p>
+            <p>0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
