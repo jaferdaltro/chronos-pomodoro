89ac1006e9d436ccc4924fd1b7a3409ac753d328
Jafer Daltro <jafer.daltro@gmail.com>
2026-02-15 10:09:45 -0300

    Fix general configurations

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,24 +1,21 @@
-import { HistoryIcon, HomeIcon, SnailIcon, SunIcon, TimerIcon } from 'lucide-react';
+import { CogIcon, HistoryIcon, HomeIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
 
 export function Menu() {
   return (
-
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href='#'>
         <HomeIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href='#'>
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href="#">
-        <SnailIcon />
+      <a className={styles.menuLink} href='#'>
+        <CogIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a className={styles.menuLink} href='#'>
         <SunIcon />
       </a>
     </nav>
-
-
-  )
+  );
 }
