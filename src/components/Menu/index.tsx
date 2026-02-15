f597dd94006260b773986c957abf286f4fdfa486
Jafer Daltro <jafer.daltro@gmail.com>
2026-02-15 14:45:18 -0300

    Configure theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,55 @@
-import { CogIcon, HistoryIcon, HomeIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HomeIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
+
+type AvailableTheme = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableTheme>('dark');
+  function handleChangeTheme(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+    setTheme(prev => (prev === 'dark' ? 'light' : 'dark'));
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para início'
+        title='Ir para início'
+      >
         <HomeIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Histórico'
+        title='Histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
-        <CogIcon />
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
+        <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar tema'
+        title='Mudar tema'
+        onClick={handleChangeTheme}
+      >
         <SunIcon />
       </a>
     </nav>
