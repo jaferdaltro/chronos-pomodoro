85850a3403ba3d67e4334f785e0fededa33e7dd6
Jafer Daltro <jafer.daltro@gmail.com>
2026-02-17 08:05:01 -0300

    Add cycle logic

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,18 +1,67 @@
 import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { useRef } from 'react';
 
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+
 export function MainForm() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const currentType = getNextCycleType(nextCycle);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!taskNameInput.current) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      duration: 1,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      type: currentType,
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
   return (
-    <form action=''>
+    <form onSubmit={handleCreateNewTask} action=''>
       <div className='formRow'>
         <DefaultInput
           id='label'
           type='text'
           labelText='task'
           placeholder='Digite algo'
+          ref={taskNameInput}
         />
       </div>
       <div className='formRow'>
